d376d32fb0c570468cc96e9c9da81da13161ed10
root <sagar437@gmail.com>
2019-09-12 19:25:02 +0000

    tictactoe first function

---
+++ tictactoe.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def create_board(board):
+	print(board[7] + ' | ' + board[8] + ' | ' + board[9])
+	print(board[4] + ' | ' + board[5] + ' | ' + board[6])
+	print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+
+#test_board = ['#','X','O','X','O','X','O','X','O','X']
+create_board(test_board)
+
